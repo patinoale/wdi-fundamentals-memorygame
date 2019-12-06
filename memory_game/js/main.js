9851574b219719b5e0e7139ae3e552d1f9534efc
Alejandra Patino <ale@alejandras-mbp.home>
2019-12-05 17:24:41 -0800

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,9 +1,27 @@
-console.log("Up and running!");
+var cards = ["queen", "queen", "king", "king"];
+var cardOne = cards[0];
+var cardTwo = cards[1];
+var cardsInPlay = [];
 
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
+cardsInPlay.push(cardOne);
+cardsInPlay.push(cardTwo);
+
 
 console.log("User flipped " + cardOne);
-console.log("User flipped " + cardFour);
+console.log("User flipped " + cardTwo);
+
+if (cardsInPlay.length === 2){
+} if (cardsInPlay[0] === cardsInPlay[1]){
+	alert("You found a match!");
+}  else {
+	alert("Sorry, try again!");
+}
+		
+
+
+
+
+
+
+
+
