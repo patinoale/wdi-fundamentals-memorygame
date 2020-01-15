ea14f1b702c8300cb2fe31796e833c724c544dae
Alejandra Patino <ale@alejandras-mbp.home>
2020-01-15 01:52:16 -0800

    created memory game

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,22 +1,33 @@
 var cards = ["queen", "queen", "king", "king"];
-var cardOne = cards[0];
-var cardTwo = cards[1];
 var cardsInPlay = [];
 
-cardsInPlay.push(cardOne);
-cardsInPlay.push(cardTwo);
-
-
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardTwo);
-
-if (cardsInPlay.length === 2){
-} if (cardsInPlay[0] === cardsInPlay[1]){
-	alert("You found a match!");
+function checkForMatch() {
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+	console.log("You found a match!");
 }  else {
-	alert("Sorry, try again!");
+	console.log("Sorry, try again!");
 }
-		
+}
+
+function flipCard(cardId){
+	console.log("User flipped " + cards[cardId]);
+	cardsInPlay.push(cards[cardId]);
+
+	if (cardsInPlay.length === 2){
+		checkForMatch();
+} 
+}
+
+flipCard(0);
+flipCard(2);
+
+
+
+
+
+
+
+
 
 
 
